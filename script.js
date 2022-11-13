f04a7d2026264dfe84ad3e807c52107eb16408a9
Gourab Raj <89293518+gouravrj@users.noreply.github.com>
2022-11-14 02:12:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,63 @@
-
-function searchCity(){
-    let city = document.getElementById("input").value
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=celcius`)
-    .then(res => res.json())
-    .then(data => {
-        // console.log(data)
-        const city_name = data.name
-        const temprature = data.main.temp
-        const min_temp = data.main.temp_min
-        const max_temp = data.main.temp_max
-        const weather_type = data.weather[0].main
-
-        document.getElementById("city").innerText = city_name
-        document.getElementById("temp").innerText = temprature
-        document.getElementById("min-temp").innerText = min_temp
-        document.getElementById("max-temp").innerText = max_temp
-        document.getElementById("weather-type").innerText = weather_type
-    })
-    .catch(err => console.log(err))
+function toggleItalics(ele){
+    ele.classList.toggle('active')
+    let text = document.getElementById("input").value
+    document.getElementById("output").classList.toggle("italics")
+    document.getElementById("output").innerText = text
 }
+function toggleBold(ele){
+    ele.classList.toggle('active')
+    let text = document.getElementById("input").value
+    document.getElementById("output").classList.toggle("bold")
+    document.getElementById("output").innerText = text
+}
+function toggleUnderline(ele){
+    ele.classList.toggle('active')
+    let text = document.getElementById("input").value
+    document.getElementById("output").classList.toggle("underline")
+    document.getElementById("output").innerText = text
+}
+
+function updateText(){
+    let textarea_text = document.getElementById("input").value
+    document.getElementById("output").innerText = textarea_text
+}
+function toggleH(ele,size)
+{
+    if(size==='0')
+    {
+        document.getElementById("output").classList.remove('fs-1','fs-2')
+        var yo = document.querySelectorAll(".h")
+        for(let i=0;i<yo.length;i++)
+            yo[i].classList.remove('active')
+        return
+    }
+    var yo = document.querySelectorAll(".h")
+    for(let i=0;i<yo.length;i++)
+        yo[i].classList.remove('active')
+    ele.classList.add('active')
+    document.getElementById("output").classList.remove('fs-1','fs-2')
+    var class_name = `fs-${size}`
+    document.getElementById("output").classList.add(class_name)
+
+}
+function align(ele,yo){
+
+    if(yo==='default')
+    {
+
+        document.getElementById("output").classList.remove('text-left','text-right','text-center')
+        var button_list = document.querySelectorAll(".align")
+        for(let i=0;i<button_list.length;i++)
+            button_list[i].classList.remove('active');
+        return
+    }
+    var button_list = document.querySelectorAll(".align")
+    for(let i=0;i<button_list.length;i++)
+        button_list[i].classList.remove('active');
+    ele.classList.add('active')
+    document.getElementById("output").classList.remove('text-left','text-right','text-center')
+    var class_name = `text-${yo}`
+    document.getElementById("output").classList.add(class_name)
+}
+
+
